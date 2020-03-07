d7a2b03f8377306f11f9e9cabd7d208816048844
Esquire <thestunnaman1@gmail.com>
2020-03-07 10:26:29 +0000

    js variables, arrays, objects

---
+++ fullStack/month1/week3/js/main.js
@@ -0,0 +1,12 @@
+console.log('hello')
+
+//variable declaration
+var name="Abena"
+let age=20
+const height=80
+
+//arrays
+var myarray=[4,5,6,7,8,'Esquire','Allen']
+
+//objects
+var myobjects={name:'Kelvin', age:20, height:80}
